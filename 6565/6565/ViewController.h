94b590ff342d32eae4740d4665cb72397e7bf5bd
tarena01 <tarena01@tangsengxitouyongpiaorou.local>
2015-07-01 11:45:39 +0800

    343453

---
+++ 6565/6565/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  6565
+//
+//  Created by tarena01 on 15-7-1.
+//  Copyright (c) 2015年 tarena01. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 6565/6565/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  6565
+//
+//  Created by tarena01 on 15-7-1.
+//  Copyright (c) 2015年 tarena01. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"asf");
+    
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
